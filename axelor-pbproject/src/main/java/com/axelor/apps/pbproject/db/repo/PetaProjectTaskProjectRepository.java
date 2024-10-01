0b719e021c1a4dc2bba2baedac32256a3a865da9
Beren Sanzharbekov <790-824@mail.ru>
2024-10-01 18:35:09 +0600

    added extra code

--- axelor-pbproject/src/main/java/com/axelor/apps/pbproject/db/repo/PetaProjectTaskProjectRepository.java
+++ axelor-pbproject/src/main/java/com/axelor/apps/pbproject/db/repo/PetaProjectTaskProjectRepository.java
@@ -4,6 +4,7 @@ import com.axelor.apps.businessproject.service.ProjectTaskProgressUpdateService;
 import com.axelor.apps.businesssupport.db.repo.ProjectTaskBusinessSupportRepository;
 import com.axelor.apps.project.db.ProjectTask;
 import com.axelor.apps.pbproject.enums.StatusNames;
+import com.axelor.apps.project.db.repo.ProjectTaskRepository;
 import com.google.inject.Inject;
 
 import java.time.LocalDate;
@@ -11,22 +12,23 @@ import java.time.LocalDateTime;
 
 public class PetaProjectTaskProjectRepository extends ProjectTaskBusinessSupportRepository {
 
+    private final ProjectTaskRepository projectTaskRepository;
     @Inject
-    public PetaProjectTaskProjectRepository(ProjectTaskProgressUpdateService projectTaskProgressUpdateService) {
+    public PetaProjectTaskProjectRepository(ProjectTaskProgressUpdateService projectTaskProgressUpdateService, ProjectTaskRepository projectTaskRepository) {
         super(projectTaskProgressUpdateService);
+        this.projectTaskRepository = projectTaskRepository;
     }
 
     @Override
     public ProjectTask save(ProjectTask projectTask) {
         String statusName = projectTask.getStatus().getName();
-
-        if (StatusNames.DONE.getStatus().equals(statusName)) {
+        if (projectTaskRepository.DONE.equals(statusName)) {
             projectTask.setEndDateTime(LocalDateTime.now());
             projectTask.setTaskEndDate(LocalDate.now());
-        } else if (StatusNames.IN_PROGRESS.getStatus().equals(statusName)) {
+        } else if (projectTaskRepository.IN_PROGRESS.equals(statusName)) {
             projectTask.setStartDateTime(LocalDateTime.now());
             projectTask.setTaskDate(LocalDate.now());
-        } else if (StatusNames.NEW.getStatus().equals(statusName)) {
+        } else if (projectTaskRepository.NEW.equals(statusName)) {
             projectTask.setEndDateTime(null);
             projectTask.setStartDateTime(null);
             projectTask.setTaskDate(null);
