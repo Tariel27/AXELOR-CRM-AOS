192b718760d4d0fbb03d5f9171bd442dfe08e0e3
tariel27 <tariel.fifa2015@gmail.com>
2024-12-12 17:24:09 +0600

    updated PublicWebController.java

--- axelor-pbproject/src/main/java/com/axelor/apps/pbproject/web/PublicWebController.java
+++ axelor-pbproject/src/main/java/com/axelor/apps/pbproject/web/PublicWebController.java
@@ -6,6 +6,8 @@ import com.google.inject.Inject;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Path("public")
 public class PublicWebController {
@@ -18,9 +20,11 @@ public class PublicWebController {
 
     @GET
     @Path("all-lunch")
+    @Produces(MediaType.APPLICATION_JSON)
     public Response checkExpenseLine() {
         Response response = new Response();
-        response.setData(lunchService.getLunch());
+        String lunch = lunchService.getLunch();
+        response.setData(lunch.isEmpty() ? "Пусто" : lunch);
         return response;
     }
 
