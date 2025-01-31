12c2d79b530853a7b09c328edc859ff93ee403c4
tariel27 <tariel.fifa2015@gmail.com>
2025-01-31 18:18:56 +0600

    updated PetaProjectTaskProjectRepository.java updated ProjectTask.xml

--- axelor-pbproject/src/main/java/com/axelor/apps/pbproject/db/repo/PetaProjectTaskProjectRepository.java
+++ axelor-pbproject/src/main/java/com/axelor/apps/pbproject/db/repo/PetaProjectTaskProjectRepository.java
@@ -1,5 +1,6 @@
 package com.axelor.apps.pbproject.db.repo;
 
+import com.axelor.apps.base.service.exception.TraceBackService;
 import com.axelor.apps.businessproject.service.ProjectTaskProgressUpdateService;
 import com.axelor.apps.businesssupport.db.repo.ProjectTaskBusinessSupportRepository;
 import com.axelor.apps.project.db.ProjectTask;
@@ -20,6 +21,7 @@ public class PetaProjectTaskProjectRepository extends ProjectTaskBusinessSupport
     @Override
     public ProjectTask save(ProjectTask projectTask) {
         updateProjectTaskDates(projectTask);
+        setFactHour(projectTask);
         return super.save(projectTask);
     }
 
@@ -40,6 +42,51 @@ public class PetaProjectTaskProjectRepository extends ProjectTaskBusinessSupport
             projectTask.setStartDateTime(LocalTime.now(ZoneId.of("UTC+6")));
             projectTask.setTaskDate(LocalDate.now());
         }
+    }
 
+    private void setFactHour(ProjectTask projectTask) {
+        if (Objects.isNull(projectTask.getTaskEndDate())) return;
+        if (Objects.isNull(projectTask.getEndDateTime())) return;
+        if (Objects.isNull(projectTask.getStartDateTime())) return;
+        if (Objects.isNull(projectTask.getTaskDate())) return;
+
+        LocalDateTime startDateTimeFull = LocalDateTime.of(projectTask.getTaskDate(), projectTask.getStartDateTime());
+        LocalDateTime endDateTimeFull = LocalDateTime.of(projectTask.getTaskEndDate(), projectTask.getEndDateTime());
+
+        Duration duration = Duration.between(startDateTimeFull, endDateTimeFull);
+        if (Objects.isNull(duration)) return;
+        long seconds = duration.getSeconds();
+        if (seconds < 0) return;
+
+        try {
+            long totalDays = seconds / (24 * 60 * 60);
+            long remainingSeconds = seconds % (24 * 60 * 60);
+            long totalHours = remainingSeconds / (60 * 60);
+            long totalMinutes = (remainingSeconds % (60 * 60)) / 60;
+            projectTask.setFactHoursString(totalDays + " дней, " + totalHours + " часов и " + totalMinutes + " минут.");
+        } catch (Exception exception) {
+            TraceBackService.trace(exception);
+        }
+
+        try {
+            long workSeconds = 0;
+            LocalDate currentDate = projectTask.getTaskDate();
+            while (!currentDate.isAfter(projectTask.getTaskEndDate())) {
+                LocalDateTime startOfDay = currentDate.atTime(9, 0);
+                LocalDateTime endOfDay = currentDate.atTime(18, 0);
+                if (currentDate.getDayOfWeek() != DayOfWeek.SATURDAY && currentDate.getDayOfWeek() != DayOfWeek.SUNDAY) {
+                    LocalDateTime actualStart = startDateTimeFull.isBefore(startOfDay) ? startOfDay : startDateTimeFull;
+                    LocalDateTime actualEnd = endDateTimeFull.isAfter(endOfDay) ? endOfDay : endDateTimeFull;
+                    if (actualStart.isBefore(actualEnd)) {
+                        Duration workDuration = Duration.between(actualStart, actualEnd);
+                        workSeconds += workDuration.getSeconds();
+                    }
+                }
+                currentDate = currentDate.plusDays(1);
+            }
+            projectTask.setFactHours((int) workSeconds);
+        } catch (Exception exception) {
+            TraceBackService.trace(exception);
+        }
     }
 }
